d3528ebab2c6f033ff218b807823194e982c2326
absZone <48597097+absZone@users.noreply.github.com>
2019-03-15 22:11:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<title><?php session_start();
 	if(isset($_SESSION['key']) && $_SESSION['type']=='admin')
 	{
-		 echo "Welcome Admin";
+		 echo "Welcome Administrator";
 	}
 	else {
 		echo "Online Classroom";
