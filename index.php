166e943d8147e0f2401026d11c0e052b32707f9a
vcadmin <absonlineclassroom@gmail.com>
2019-03-15 22:32:24 +0530

    TT

--- index.php
+++ index.php
@@ -1,67 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title><?php session_start();
-	if(isset($_SESSION['key']) && $_SESSION['type']=='admin')
-	{
-		 echo "Welcome Admin";
-	}
-	else {
-		echo "Online Classroom";
-	}
-	 ?></title>
-	
-	<meta name="description" content="" />
-	<meta name="keywords" content="" />
-	<meta name="author" content="" />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php session_start();
+						if (isset($_SESSION['key']) && $_SESSION['type'] == 'admin') {
+								echo "Welcome Admin";
+							} else {
+							echo "Online Classroom";
+						}
+						?></title>
 
-  <!-- Facebook and Twitter integration -->
-	<meta property="og:title" content=""/>
-	<meta property="og:image" content=""/>
-	<meta property="og:url" content=""/>
-	<meta property="og:site_name" content=""/>
-	<meta property="og:description" content=""/>
-	<meta name="twitter:title" content="" />
-	<meta name="twitter:image" content="" />
-	<meta name="twitter:url" content="" />
-	<meta name="twitter:card" content="" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
 
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,800" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700" rel="stylesheet">
+    <!-- Facebook and Twitter integration -->
+    <meta property="og:title" content="" />
+    <meta property="og:image" content="" />
+    <meta property="og:url" content="" />
+    <meta property="og:site_name" content="" />
+    <meta property="og:description" content="" />
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:image" content="" />
+    <meta name="twitter:url" content="" />
+    <meta name="twitter:card" content="" />
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,800" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700" rel="stylesheet">
 
 
 
-	
-	<!-- Animate.css -->
-	<link rel="stylesheet" href="css/animate.css">
-	<!-- Icomoon Icon Fonts-->
-	<link rel="stylesheet" href="css/icomoon.css">
-	<!-- Bootstrap  -->
-	<link rel="stylesheet" href="css/bootstrap.css">
 
-	<!-- Magnific Popup -->
-	<link rel="stylesheet" href="css/magnific-popup.css">
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="css/animate.css">
+    <!-- Icomoon Icon Fonts-->
+    <link rel="stylesheet" href="css/icomoon.css">
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="css/bootstrap.css">
 
-	<!-- Flexslider  -->
-	<link rel="stylesheet" href="css/flexslider.css">
+    <!-- Magnific Popup -->
+    <link rel="stylesheet" href="css/magnific-popup.css">
 
-	<!-- Owl Carousel -->
-	<link rel="stylesheet" href="css/owl.carousel.min.css">
-	<link rel="stylesheet" href="css/owl.theme.default.min.css">
-	
-	<!-- Flaticons  -->
-	<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <!-- Flexslider  -->
+    <link rel="stylesheet" href="css/flexslider.css">
 
-	<!-- Theme style  -->
-	<link rel="stylesheet" href="css/style.css">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
 
-	<!-- Modernizr JS -->
-	<script src="js/modernizr-2.6.2.min.js"></script>
-	<!-- FOR IE9 below -->
-	<!--[if lt IE 9]>
+    <!-- Flaticons  -->
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <!-- Theme style  -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Modernizr JS -->
+    <script src="js/modernizr-2.6.2.min.js"></script>
+    <!-- FOR IE9 below -->
+    <!--[if lt IE 9]>
 	<script src="js/respond.min.js"></script>
 	<![endif]-->
 
@@ -69,119 +68,120 @@
 </head>
 
 <body>
-<div id="wrapper">
-<div>
-			<?php include('elements/header.php'); ?>
-					</div>
-        
-				<div id="page-content-wrapper">
-				
-				<button type="button" class="hamburger is-closed" data-toggle="offcanvas">
+    <div id="wrapper">
+        <div>
+            <?php include('elements/header.php'); ?>
+        </div>
+
+        <div id="page-content-wrapper">
+
+            <button type="button" class="hamburger is-closed" data-toggle="offcanvas">
                 <span class="hamb-top"></span>
-    			<span class="hamb-middle"></span>
-				<span class="hamb-bottom"></span>
-						</button>
+                <span class="hamb-middle"></span>
+                <span class="hamb-bottom"></span>
+            </button>
 
-<div class="colorlib-blog colorlib-light-grey">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-8 col-md-offset-2 text-center colorlib-heading animate-box">
-						<h2><a href=news.php> News</a></h2>
-						<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name</p>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-6 animate-box">
-						<article class="article-entry">
-							<a href="blog.html" class="blog-img" style="background-image: url(images/blog-1.jpg);">
-								<p class="meta"><span class="day">18</span><span class="month">Apr</span></p>
-							</a>
-							<div class="desc">
-								<h2><a href="blog.html">Creating Mobile Apps</a></h2>
-								<p class="admin"><span>Posted by:</span> <span>James Smith</span></p>
-								<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life. The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way.</p>
-							</div>
-						</article>
-					</div>
-					<div class="col-md-6">
-						<div class="f-blog animate-box">
-							<a href="blog.html" class="blog-img" style="background-image: url(images/blog-1.jpg);">
-							</a>
-							<div class="desc">
-								<h2><a href="blog.html">How to Create Website in Scratch</a></h2>
-								<p class="admin"><span>04 March 2018</span></p>
-								<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life</p>
-							</div>
-						</div>
-						<div class="f-blog animate-box">
-							<a href="blog.html" class="blog-img" style="background-image: url(images/blog-2.jpg);">
-							</a>
-							<div class="desc">
-								<h2><a href="blog.html">How to Convert PSD File to HTML File?</a></h2>
-								<p class="admin"><span>04 March 2018</span></p>
-								<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life</p>
-							</div>
-						</div>
-						<div class="f-blog animate-box">
-							<a href="blog.html" class="blog-img" style="background-image: url(images/blog-3.jpg);">
-							</a>
-							<div class="desc">
-								<h2><a href="blog.html">How to Build Games App in Mobile</a></h2>
-								<p class="admin"><span>04 March 2018</span></p>
-								<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+            <div class="colorlib-blog colorlib-light-grey">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 col-md-offset-2 text-center colorlib-heading animate-box">
+                            <h2><a href=news.php> News</a></h2>
+                            <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 animate-box">
+                            <article class="article-entry">
+                                <a href="blog.html" class="blog-img" style="background-image: url(images/blog-1.jpg);">
+                                    <p class="meta"><span class="day">18</span><span class="month">Apr</span></p>
+                                </a>
+                                <div class="desc">
+                                    <h2><a href="blog.html">Creating Mobile Apps</a></h2>
+                                    <p class="admin"><span>Posted by:</span> <span>James Smith</span></p>
+                                    <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life. The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way.</p>
+                                </div>
+                            </article>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="f-blog animate-box">
+                                <a href="blog.html" class="blog-img" style="background-image: url(images/blog-1.jpg);">
+                                </a>
+                                <div class="desc">
+                                    <h2><a href="blog.html">How to Create Website in Scratch</a></h2>
+                                    <p class="admin"><span>04 March 2018</span></p>
+                                    <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life</p>
+                                </div>
+                            </div>
+                            <div class="f-blog animate-box">
+                                <a href="blog.html" class="blog-img" style="background-image: url(images/blog-2.jpg);">
+                                </a>
+                                <div class="desc">
+                                    <h2><a href="blog.html">How to Convert PSD File to HTML File?</a></h2>
+                                    <p class="admin"><span>04 March 2018</span></p>
+                                    <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life</p>
+                                </div>
+                            </div>
+                            <div class="f-blog animate-box">
+                                <a href="blog.html" class="blog-img" style="background-image: url(images/blog-3.jpg);">
+                                </a>
+                                <div class="desc">
+                                    <h2><a href="blog.html">How to Build Games App in Mobile</a></h2>
+                                    <p class="admin"><span>04 March 2018</span></p>
+                                    <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
-<div>
-<?php include('elements/footer.php'); ?>
-</div>
+    <div>
+        <?php include('elements/footer.php'); ?>
+    </div>
 
-</div>
-        <!-- /#page-content-wrapper -->
-</div>
+    </div>
+    <!-- /#page-content-wrapper -->
+    </div>
     <!-- /#wrapper -->
 
-		
-<!-- jQuery -->
-<script src="js/jquery.min.js"></script>
-	<!-- jQuery Easing -->
-	<script src="js/jquery.easing.1.3.js"></script>
-	<!-- Bootstrap -->
-	<script type="text/javascript" src="js/materialize.min.js"></script>
 
-	<script src="js/bootstrap.min.js"></script>
-	<!-- Waypoints -->
-	<script src="js/jquery.waypoints.min.js"></script>
-	<!-- Stellar Parallax -->
-	<script src="js/jquery.stellar.min.js"></script>
-	<!-- Flexslider -->
-	<script src="js/jquery.flexslider-min.js"></script>
-	<!-- Owl carousel -->
-	<script src="js/owl.carousel.min.js"></script>
-	<!-- Magnific Popup -->
-	<script src="js/jquery.magnific-popup.min.js"></script>
-	<script src="js/magnific-popup-options.js"></script>
-	<!-- Counters -->
-	<script src="js/jquery.countTo.js"></script>
-	<!-- Main -->
-	<script src="js/main.js"></script>
+    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+    <!-- jQuery Easing -->
+    <script src="js/jquery.easing.1.3.js"></script>
+    <!-- Bootstrap -->
+    <script type="text/javascript" src="js/materialize.min.js"></script>
 
-	<script src="js/main_side.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Waypoints -->
+    <script src="js/jquery.waypoints.min.js"></script>
+    <!-- Stellar Parallax -->
+    <script src="js/jquery.stellar.min.js"></script>
+    <!-- Flexslider -->
+    <script src="js/jquery.flexslider-min.js"></script>
+    <!-- Owl carousel -->
+    <script src="js/owl.carousel.min.js"></script>
+    <!-- Magnific Popup -->
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/magnific-popup-options.js"></script>
+    <!-- Counters -->
+    <script src="js/jquery.countTo.js"></script>
+    <!-- Main -->
+    <script src="js/main.js"></script>
 
-	<script>
-$(document).ready(function(){
-  $('.dropdown-submenu a.test').on("click", function(e){
-    $(this).next('ul').toggle();
-    e.stopPropagation();
-    e.preventDefault();
-  });
-});
-</script>
+    <script src="js/main_side.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('.dropdown-submenu a.test').on("click", function(e) {
+                $(this).next('ul').toggle();
+                e.stopPropagation();
+                e.preventDefault();
+            });
+        });
+    </script>
 
 </body>
-</html>
+
+</html> 
