a05308614842ca902a60592e1e513e7ac979dc82
Virtual Campus <contentshare0@gmail.com>
2019-03-30 00:14:21 +0530

    delete old

--- scripts/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$con=mysqli_connect('localhost','root','','onlineclassroom')
-// if(!con){
-//     die('connection Failed');
-// }
-// else{
-//     echo 'successful';
-// }
-?>
--- scripts/globalVar.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('db.php');
-$isLogIn = isset($_SESSION['key']);
-if (isset($_SESSION['key'])) {
-    $id = $_SESSION['key'];
-    $loginType = $_SESSION['type'];
-}
-?>
