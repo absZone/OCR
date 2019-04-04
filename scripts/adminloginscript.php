f3a00b6a94da92d94a1df64b892334f3cf3a9991
SNEHA MANE <absonlineclassroom@gmail.com>
2019-04-04 22:02:19 +0530

    shweta

--- scripts/adminloginscript.php
+++ scripts/adminloginscript.php
@@ -5,19 +5,33 @@ $loginId=mysqli_real_escape_string($con,$_POST['loginId']);
 $password=mysqli_real_escape_string($con,$_POST['password']);
 $encryptPassword=md5($password);
 $loginQuery="select * from admin WHERE loginId='$loginId' and password='$encryptPassword'";
-$result=mysqli_query($con,$loginQuery);
-if(mysqli_num_rows($result)>0)
-{
-    session_start();
-    while($row=mysqli_fetch_assoc($result))
-    {
-        echo "adminId= ".$row["adminId"];
-        $_SESSION['key']=$row["adminId"];
-        $_SESSION['type']='admin';
-    }
-    header("Location:../index.php");
-}
-else{
-    header("Location:../adminLogin.php");
-}
+// $result=mysqli_query($con,$loginQuery);
+// if(mysqli_num_rows($result)>0)
+// {
+//     session_start();
+//     while($row=mysqli_fetch_assoc($result))
+//     {
+//         echo "adminId= ".$row["adminId"];
+//         $_SESSION['key']=$row["adminId"];
+//         $_SESSION['type']='admin';
+//     }
+//     // header("Location:../index.php");
+// }
+// else{
+//     header("Location:../adminLogin.php");
+// }
+if(mysqli_query($con,$loginQuery))
+	{
+            $msg = 1;
+
+	}
+	else
+	{
+            $msg = 3;
+	
+       
+        
+	}
+
+echo $msg;
 ?>
