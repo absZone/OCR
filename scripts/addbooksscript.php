a9945da96f60ef9591d6c278444d5122cf9dbb36
unknown <absonlineclassroom@gmail.com>
2019-04-18 17:12:03 +0530

    shweta

--- scripts/addbooksscript.php
+++ scripts/addbooksscript.php
@@ -1,21 +1,15 @@
 <?php
-require('db.php');
-
-
-
-if (isset($_POST['submit'])) {
-        $bookName = $_FILES['file']['bookName'];
-        $temp = $_FILES['file']['tmp_name'];
-        $courseId = mysqli_real_escape_string($con, $_POST['courseId']);
-
-        move_uploaded_file($temp, "uploadvideo/", $bookName);
-        $query = "INSERT INTO 'video'('bookId','courseId','bookName') VALUES ('UUID','$courseId','$bookName')";
-        if (mysqli_query($con, $query)) {
-                echo "submitted to database";
-            }
-
-        {
-            echo "" . $bookName . "has been submitted";
-        }
-    }
- ?>
+if(isset($_POST['btn-upload']))
+{    
+     
+ $file = rand(1000,100000)."-".$_FILES['file']['name'];
+    $file_loc = $_FILES['file']['tmp_name'];
+ $file_size = $_FILES['file']['size'];
+ $file_type = $_FILES['file']['type'];
+ $folder="uploads/";
+ 
+ move_uploaded_file($file_loc,$folder.$file);
+ $sql="INSERT INTO tbl_uploads(file,type,size) VALUES('$file','$file_type','$file_size')";
+ mysql_query($sql); 
+}
+?>
