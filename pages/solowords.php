f34fa6dd4b3fd480baac354494a315e7bf7aeb71
Bharat Mali <absonlineclassroom@gmail.com>
2019-04-20 06:12:24 +0530

    bharat

---
+++ pages/solowords.php
@@ -0,0 +1,68 @@
+<?php session_start();
+require('../scripts/db.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Neon Admin Panel" />
+    <meta name="author" content="" />
+
+    <link rel="icon" href="../assets/images/favicon.ico">
+
+    <title>Online Classroom | Games</title>
+
+    <link rel="stylesheet" href="../assets/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css">
+    <link rel="stylesheet" href="../assets/css/font-icons/entypo/css/entypo.css">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic">
+    <link rel="stylesheet" href="../assets/css/bootstrap.css">
+    <link rel="stylesheet" href="../assets/css/neon-core.css">
+    <link rel="stylesheet" href="../assets/css/neon-theme.css">
+    <link rel="stylesheet" href="../assets/css/neon-forms.css">
+    <link rel="stylesheet" href="../assets/css/custom.css">
+
+    <script src="../assets/js/jquery-1.11.3.min.js"></script>
+
+    <!--[if lt IE 9]><script src="../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+
+</head>
+
+<body class="page-body" data-url="http://neon.dev">
+
+    <div class="page-container">
+        <!-- add class "sidebar-collapsed" to close sidebar by default, "chat-visible" to make chat appear always -->
+
+        <?php include('../elements/header.php'); ?>
+
+        <div class="main-content">
+            <?php include('../elements/tobbar.php'); ?>
+            <hr />
+
+            <ol class="breadcrumb bc-3">
+                <li>
+                    <a href="#"><i class="fa-home"></i>Home</a>
+                </li>
+                <li>
+                    <a href="#">GAMES</a>
+                </li>
+                
+            </ol>
+            <h1>SOLO WORDS</h1>
+            <br>
+            <div><a href='http://www.willinggames.com/puzzle-games/Solo-Words.html' style='color:#2e4b82;'><img src='http://www.willinggames.com/gamespic/Solo-Words.jpg' width='50' height='50' align='left' style='float:left; border:2px solid #006; margin-right:5px;'><b style='display:block;padding-top:18px;'>Solo Words</b></a><br clear='all'></div>
+            <embed src='http://www.willinggames.com/flash/Solo-Words.swf' type='application/x-shockwave-flash' width='640' height='480'><embed/><br><a href='http://www.willinggames.com' target='_blank' title='Small Flash Games'>Play Free Flash Games at WillingGames</a>
+</body>
+
+</html> 
